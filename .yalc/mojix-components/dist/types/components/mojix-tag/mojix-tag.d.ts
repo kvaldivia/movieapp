0a4e474e393ffe73beb2c4b134ca3a8128d3efb3
Kenny Valdivia <k_valdivia@gmx.com>
2020-12-21 01:59:38 -0500

    Yalc build files removed

--- .yalc/mojix-components/dist/types/components/mojix-tag/mojix-tag.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class MojixTag {
-  icon: string;
-  link: string;
-  disable: boolean;
-  render(): any;
-  private renderBaseTemplate;
-  private renderTemplateWithIcon;
-  private renderTemplate;
-}
