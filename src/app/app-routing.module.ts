f3aac4d743f30f36cbff37e5fa1aa360dfe2259b
Kenny Valdivia <k_valdivia@gmx.com>
2021-01-03 01:03:53 -0500

    Movie details page boilerplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'movie',
+    loadChildren: () => import('./movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   }
 ];
 @NgModule({
